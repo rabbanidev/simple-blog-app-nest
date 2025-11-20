fb246c14966df68cd6bda8ac2b767332db15d2a5
rabbanidev <golamrabbani227417@gmail.com>
2025-11-20 19:23:58 +0600

    Comment update and delete api

--- src/modules/features/comment/comment.controller.ts
+++ src/modules/features/comment/comment.controller.ts
@@ -1,9 +1,18 @@
-import { Body, Controller, Param, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { AuthGuard } from 'src/modules/core/auth/auth.guard';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import type { AuthRequest } from 'src/interfaces';
+import { UpdateCommentDto } from './dto/update-comment.dto';
 
 @Controller('/comments')
 export class CommentController {
@@ -23,5 +32,35 @@ export class CommentController {
       req.user,
     );
     return { message: 'Comment create successfully', data: result };
+  }
+
+  @ApiBearerAuth('access-token')
+  @UseGuards(AuthGuard)
+  @Post('/:blogId/update/:commentId')
+  async updateComment(
+    @Param('blogId') blogId: string,
+    @Param('commentId') commentId: string,
+    @Body() updateCommentDto: UpdateCommentDto,
+    @Req() req: AuthRequest,
+  ) {
+    const result = await this.commentService.updateComment(
+      blogId,
+      commentId,
+      updateCommentDto,
+      req.user,
+    );
+    return { message: 'Comment update successfully', data: result };
+  }
+
+  @ApiBearerAuth('access-token')
+  @UseGuards(AuthGuard)
+  @Delete('/:blogId/delete/:commentId')
+  async deleteComment(
+    @Param('blogId') blogId: string,
+    @Param('commentId') commentId: string,
+    @Req() req: AuthRequest,
+  ) {
+    await this.commentService.deleteComment(blogId, commentId, req.user);
+    return { message: 'Comment deleted successfully' };
   }
 }
--- src/modules/features/comment/comment.service.ts
+++ src/modules/features/comment/comment.service.ts
@@ -1,10 +1,15 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { Comment, CommentDocument } from './schemas/comment.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { BlogService } from '../blog/blog.service';
 import { JWTPayloadUser } from 'src/interfaces';
+import { UpdateCommentDto } from './dto/update-comment.dto';
 
 @Injectable()
 export class CommentService {
@@ -48,5 +53,65 @@ export class CommentService {
 
     const result = await this.blogService.getBlog(blogId);
     return result;
+  }
+
+  async updateComment(
+    blogId: string,
+    commentId: string,
+    updateCommentDto: UpdateCommentDto,
+    loginUser: JWTPayloadUser,
+  ) {
+    const updatedComment = await this.commentModel.findOneAndUpdate(
+      {
+        _id: commentId,
+        user: loginUser.userId,
+      },
+      updateCommentDto,
+    );
+
+    if (!updatedComment) {
+      throw new NotFoundException('Comment not found');
+    }
+
+    const result = await this.blogService.getBlog(blogId);
+    return result;
+  }
+
+  async deleteComment(
+    blogId: string,
+    commentId: string,
+    loginUser: JWTPayloadUser,
+  ) {
+    const comment = await this.commentModel.findOne({
+      _id: commentId,
+      user: loginUser.userId,
+    });
+
+    if (!comment) {
+      throw new NotFoundException('Comment not found');
+    }
+
+    const blog = await this.blogService.getBlog(blogId);
+
+    const newComments = blog.comments.filter(
+      (c) => String(c._id) !== String(commentId),
+    );
+    blog.comments = newComments;
+
+    const session = await this.commentModel.startSession();
+    session.startTransaction();
+
+    try {
+      await this.commentModel.findByIdAndDelete(commentId, { session });
+
+      await blog.save({ session });
+
+      await session.commitTransaction();
+      await session.endSession();
+    } catch (error) {
+      await session.abortTransaction();
+      await session.endSession();
+      throw error;
+    }
   }
 }
